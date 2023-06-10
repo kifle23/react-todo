a08abd76aafe50736eb0ce1e07679f51fedd428c
kifle23 <68891198+kifle23@users.noreply.github.com>
2023-06-10 14:36:00 +0300

    Add CSS - part7

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,25 @@
+import styles from '@/styles/TodoItem.module.css';
+
 const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
   return (
-    <li>
-      <input
-        type="checkbox"
-        checked={itemProp.completed}
-        onChange={() => handleChange(itemProp.id)}
-      />
-      <button onClick={() => delTodo(itemProp.id)}>Delete</button>
-      {itemProp.title}
+    <li className={styles.item}>
+      <div className={styles.content}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+        <span style={itemProp.completed ? completedStyle : null}>
+          {itemProp.title}
+        </span>
+      </div>
     </li>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import TodoApp from '@/components/TodoApp';
 import Navbar from '@/components/Navbar';
-import Modal from './components/Modal';
+// import Modal from './components/Modal';
+import '@/styles/app.css';
 
 const domContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(domContainer);
@@ -11,6 +11,6 @@ root.render(
   <React.StrictMode>
     <Navbar></Navbar>
     <TodoApp />
-    <Modal />
+    {/* <Modal /> */}
   </React.StrictMode>
 );
